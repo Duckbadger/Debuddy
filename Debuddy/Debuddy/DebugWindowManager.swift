6923e6f95583527baeee1b663d11863a87687332
ken <ken@kensfrequency.com>
2017-08-21 22:21:47 +0100

    DebugWindowManager

---
+++ Debuddy/Debuddy/DebugWindowManager.swift
@@ -0,0 +1,17 @@
+//
+//  DebugWindowManager.swift
+//  Debuddy
+//
+//  Created by Ken on 17/08/2017.
+//  Copyright © 2017 Ken Boucher. All rights reserved.
+//
+
+import Foundation
+
+class DebugWindowManager {
+  
+  let shared = DebugWindowManager()
+  
+  
+  
+}
